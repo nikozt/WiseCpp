bc88f62cfdc06632591b8b080b8a0a270c722891
nikozt <ivantoe@yandex.ru>
2019-09-25 17:14:56 +0300

    Added some new code.

--- test/test.cpp
+++ test/test.cpp
@@ -2,12 +2,12 @@
 #include <iostream>
 #include "TestMemory.h"
 #include "TestMemory3.h"
-
+#include "TestMemory4.h"
 
 int main()
 {
     std::cout << "Hello World!\n";
 	//int result = TestMemory();
-	int result = TestMemory();
+	int result = TestMemory4();
 }
 
